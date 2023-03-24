e806c25bdddf4346d9b0a58e0095607369be306c
imrbpro <mrb5only>
2023-03-24 09:33:20 +0500

    Implementation of Singleton Design Pattern

--- TheDesignPatterns/CreationalPatterns/Singleton.cs
+++ TheDesignPatterns/CreationalPatterns/Singleton.cs
@@ -1,12 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TheDesignPatterns.CreationalPatterns
 {
     class Singleton
     {
+        public static Singleton _singletonObject;
+        public static int ObjectCount = 0;
+        private Singleton()
+        {
+
+        }
+        public static Singleton GetSingletonObject()
+        {
+            if (_singletonObject == null)
+            {
+                _singletonObject = new Singleton();
+                ObjectCount += 1;
+            }
+            return _singletonObject;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(" Object Created " + ObjectCount + " Times");
+        }
     }
 }
--- TheDesignPatterns/Program.cs
+++ TheDesignPatterns/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TheDesignPatterns.CreationalPatterns;
 
 namespace TheDesignPatterns
 {
@@ -10,6 +11,13 @@ namespace TheDesignPatterns
     {
         static void Main(string[] args)
         {
+            Singleton singleton = Singleton.GetSingletonObject();
+            singleton.Print();
+            Singleton singleton1 = Singleton.GetSingletonObject();
+            singleton1.Print();
+            Singleton singleton2 = Singleton.GetSingletonObject();
+            singleton2.Print();
+            Console.Read();
         }
     }
 }
